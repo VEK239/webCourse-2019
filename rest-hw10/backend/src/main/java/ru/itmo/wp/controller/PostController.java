26f281a82c11d70bf0323c8f26d4eb8747754ed6
Elizaveta Vlasova <vlasova.elizaveta@yandex.ru>
2019-12-19 19:00:06 +0300

    Added hw10 1, 2

---
+++ rest-hw10/backend/src/main/java/ru/itmo/wp/controller/PostController.java
@@ -0,0 +1,26 @@
+package ru.itmo.wp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.itmo.wp.domain.Post;
+import ru.itmo.wp.domain.User;
+import ru.itmo.wp.service.PostService;
+import ru.itmo.wp.service.UserService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/1")
+public class PostController extends ApiController {
+    private final PostService postService;
+
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
+
+    @GetMapping("posts")
+    public List<Post> findPosts() {
+        return postService.findAll();
+    }
+}
---
+++ rest-hw10/frontend/src/components/middle/Users.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="datatable">
+        <div class="caption">Users</div>
+        <table>
+            <thead>
+            <tr>
+                <th>ID</th>
+                <th>Login</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr v-for="user in users" :key="user.id">
+                <td>{{user.id}}</td>
+                <td>{{user.login}}</td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Users",
+        props: ['users']
+    }
+</script>
+
+<style scoped>
+
+</style>
